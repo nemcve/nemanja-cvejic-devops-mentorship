8a7e5cf3d693e3c99dadf6252468a7999f8b2e34
Nemanja Cvejic <37965912+nemcve@users.noreply.github.com>
2023-07-07 22:45:07 +0200

    Delete task-3/chapter-16 directory

--- task-3/chapter-16/67.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test using at command
-#
-echo "This script ran at $(date +%B%d,%T)" > ./files/test78.out
-echo >> ./files/test78.out
-sleep 5
-echo "This is the script's end..." >> ./files/test78.out
-#
