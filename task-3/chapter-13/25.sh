a4574b5375fa44c4585fd3df508de513e018adec
Nemanja Cvejic <37965912+nemcve@users.noreply.github.com>
2023-07-07 22:44:25 +0200

    Delete task-3/chapter-13 directory

--- task-3/chapter-13/25.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# process new user accounts
-input="./files/users.csv"
-while IFS=',' read -r userid name
-do
-echo "adding $userid"
-useradd -c "$name" -m $userid
-done < "$input"
--- task-3/chapter-13/3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for test in Nevada "New Hampshire" "New Mexico" "New York"
-do
-echo "Now going to $test"
-done
